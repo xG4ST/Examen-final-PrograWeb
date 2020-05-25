57b9f717bf9dbb1b2136656599f5ceaf2083f508
xG4ST <gus64_gast@outlook.es>
2020-05-25 16:44:22 -0500

    Aber

--- cancelFlightPage.php
+++ cancelFlightPage.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   
-  <title>Add Flights</title>
+  <title>Añadir vuelo</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
